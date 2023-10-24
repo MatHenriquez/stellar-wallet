4fa8c189e7bae7ead895e485c25d137dd1dbd3ba
Matías Henríquez <matias.henriquez.dev@gmail.com>
2023-10-24 17:36:56 -0300

    Increase left margin for the wallet brand on home page

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Index() {
       {/* Brand */}
       <nav className="relative flex w-full flex-wrap items-center justify-between py-2 text-neutral-500 shadow-lg hover:text-neutral-700 focus:text-neutral-700  lg:py-4">
         <div className="flex w-full flex-wrap items-center justify-between px-3">
-          <div className="ml-2">
+          <div className="ml-10">
             <a
               className="text-xl font-semibold text-neutral-800 dark:text-neutral-200"
               href="#"
