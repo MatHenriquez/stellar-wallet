3e5885b373ba6a66b5ecbfa659e4eb6652021d4f
Matías Henríquez <matias.henriquez.dev@gmail.com>
2023-10-24 17:21:42 -0300

    Add wallet brand to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,19 @@
-export default function Index(){
-    return(
-        <div className="h-screen bg-cyan-950">
-
+export default function Index() {
+  return (
+    <div className="h-screen bg-cyan-950">
+      {/* Brand */}
+      <nav className="relative flex w-full flex-wrap items-center justify-between py-2 text-neutral-500 shadow-lg hover:text-neutral-700 focus:text-neutral-700  lg:py-4">
+        <div className="flex w-full flex-wrap items-center justify-between px-3">
+          <div className="ml-2">
+            <a
+              className="text-xl font-semibold text-neutral-800 dark:text-neutral-200"
+              href="#"
+            >
+              My_Wallet
+            </a>
+          </div>
         </div>
-    )
+      </nav>
+    </div>
+  );
 }
