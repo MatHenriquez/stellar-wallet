0f25b286ba0317fee5027cfc4066e1149f7e9c90
Matías Henríquez <matias.henriquez.dev@gmail.com>
2023-10-25 15:52:14 -0300

    Declare and export the InfoModal functional component in InfoModal.tsx

--- app/components/InfoModal.tsx
+++ app/components/InfoModal.tsx
@@ -2,3 +2,11 @@
 
 import { FC } from "react";
 import { IKeyPair } from "../interfaces/IKeyPair";
+
+const InfoModal: FC<IKeyPair> = ({ publicKey, secretKey }) => {
+    return (
+        <div></div>
+    );
+};
+
+export default InfoModal;
