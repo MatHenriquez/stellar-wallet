a22200859b5c709478cb5a7894a4fa40bcbbd1d4
Matías Henríquez <matias.henriquez.dev@gmail.com>
2023-10-25 10:15:10 -0300

    Remove unnecessary comments

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
 export default function Index() {
   return (
     <div className="grid grid-cols-1 grid-rows-6 h-screen bg-cyan-950">
-        
-      {/* Brand */}
       <nav className="relative flex w-full flex-wrap items-center justify-between py-2 text-neutral-500 shadow-lg hover:text-neutral-700 focus:text-neutral-700  lg:py-4">
         <div className="flex w-full flex-wrap items-center justify-between px-3">
           <div className="ml-10">
@@ -16,17 +14,17 @@ export default function Index() {
         </div>
       </nav>
 
-      {/* Login */}
       <div className="grid grid-cols-1 md:grid-cols-2 place-items-center row-span-5">
-
-        {/* Sign in button */}
         <div className="relative mb-12 px-3 lg:mb-0">
-          <a className="cursor-pointer underline underline-offset-8">Sign In with your Secret Key</a>
+          <a className="cursor-pointer underline underline-offset-8">
+            Sign In with your Secret Key
+          </a>
         </div>
 
-        {/* Sign up button */}
         <div className="relative mb-12 px-3 lg:mb-0">
-          <a className="cursor-pointer underline underline-offset-8">Create new keys and Sign Up</a>
+          <a className="cursor-pointer underline underline-offset-8">
+            Create new keys and Sign Up
+          </a>
         </div>
       </div>
     </div>
