36188bbd8114c367e95ffcb0a31e7ca3b1d78418
Matías Henríquez <matias.henriquez.dev@gmail.com>
2023-10-25 15:57:15 -0300

    Return the modal with the keys

--- app/components/InfoModal.tsx
+++ app/components/InfoModal.tsx
@@ -4,9 +4,57 @@ import { FC } from "react";
 import { IKeyPair } from "../interfaces/keys";
 
 const InfoModal: FC<IKeyPair> = ({ publicKey, secretKey }) => {
-    return (
-        <div></div>
-    );
+  return (
+    <div
+      data-te-modal-init
+      className="fixed left-0 top-0 z-[1055] hidden h-full w-full overflow-y-auto overflow-x-hidden outline-none"
+      id="InfoModal"
+      tabIndex={-1}
+      aria-labelledby="exampleModalCenterTitle"
+      aria-modal="true"
+      role="dialog"
+    >
+      <div
+        data-te-modal-dialog-ref
+        className="pointer-events-none relative flex min-h-[calc(100%-1rem)] w-auto translate-y-[-50px] items-center opacity-0 transition-all duration-300 ease-in-out min-[576px]:mx-auto min-[576px]:mt-7 min-[576px]:min-h-[calc(100%-3.5rem)] min-[576px]:max-w-[500px]"
+      >
+        <div className="pointer-events-auto relative flex w-full flex-col rounded-md border-none bg-white bg-clip-padding text-current shadow-lg outline-none dark:bg-neutral-600">
+          <div className="flex flex-shrink-0 items-center justify-between rounded-t-md border-b-2 border-neutral-100 border-opacity-100 p-4 dark:border-opacity-50">
+            <h5
+              className="text-xl font-medium leading-normal text-neutral-800 dark:text-neutral-200"
+              id="exampleModalCenterTitle"
+            >
+              Keys
+            </h5>
+
+            <button
+              type="button"
+              className="box-content rounded-none border-none hover:no-underline hover:opacity-75 focus:opacity-100 focus:shadow-none focus:outline-none"
+              data-te-modal-dismiss
+              aria-label="Close"
+            ></button>
+          </div>
+
+          <div className="relative p-4">
+            <p>Public Key: {publicKey}</p>
+            <p>Secret Key: {secretKey}</p>
+          </div>
+
+          <div className="flex flex-shrink-0 flex-wrap items-center justify-end rounded-b-md border-t-2 border-neutral-100 border-opacity-100 p-4 dark:border-opacity-50">
+            <button
+              type="button"
+              className="inline-block rounded bg-primary-100 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-primary-700 transition duration-150 ease-in-out hover:bg-primary-accent-100 focus:bg-primary-accent-100 focus:outline-none focus:ring-0 active:bg-primary-accent-200"
+              data-te-modal-dismiss
+              data-te-ripple-init
+              data-te-ripple-color="light"
+            >
+              Close
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default InfoModal;
