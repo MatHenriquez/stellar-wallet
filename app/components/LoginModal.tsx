d3b58a5b91650127cff882af3d06ffeab0ad99e5
Matías Henríquez <matias.henriquez.dev@gmail.com>
2023-10-26 11:47:24 -0300

    Delete unused useState import in LoginModal.tsx

--- app/components/LoginModal.tsx
+++ app/components/LoginModal.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { FC, useState } from "react";
+import { FC } from "react";
 
 const LoginModal: FC<{ showModal: boolean; setShowModal: (value: boolean) => void }> = ({ showModal, setShowModal }) => {
 
