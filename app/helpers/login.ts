08712994c8e62c591407636170fc6b662a434fb6
Matías Henríquez <matias.henriquez.dev@gmail.com>
2023-10-27 12:11:50 -0300

    Remove the query selector

--- app/helpers/login.ts
+++ app/helpers/login.ts
@@ -4,8 +4,6 @@ function getPublicKey(secretKey: string): string {
   try {
     return Keypair.fromSecret(secretKey).publicKey();
   } catch (error) {
-    const errorTag = document.querySelector("#error");
-    if(errorTag) errorTag.textContent = "Invalid secret key";
     throw new Error("Invalid secret key");
   }
 }
