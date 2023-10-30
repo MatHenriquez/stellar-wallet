cb740890606783da876326c4c5e4d490f7c030e5
Matías Henríquez <matias.henriquez.dev@gmail.com>
2023-10-30 11:14:38 -0300

    Change the home test to follow the new IDs in kebab-case

--- app/page.cy.tsx
+++ app/page.cy.tsx
@@ -5,34 +5,34 @@ describe("Home", () => {
     cy.mount(<Home />);
   });
 
-  it("should have the wallet brand", () => {
+  it("Should have the wallet brand", () => {
     cy.get('[href="#"]').should("have.text", "My_Wallet");
   });
 
-  it("should have a link to sign in with a secret key", () => {
+  it("Should have a link to sign in with a secret key", () => {
     cy.contains('Sign In with your Secret Key')
   });
 
-  it("should have a link to create new keys", () => {
+  it("Should have a link to create new keys", () => {
     cy.get('[data-te-toggle="modal"]').should(
       "have.text",
       "Create new keys and Sign Up"
     );
   });
 
-  it("should show the keys when clicking the create new keys link", () => {
-    cy.get('[data-te-target="#InfoModal"]').click();
+  it("Should show the keys when clicking the create new keys link", () => {
+    cy.get('[data-te-target="#info-modal"]').click();
     cy.contains("Public Key: ");
     cy.contains("Secret Key: ");
   });
 
-  it("should show the login modal when clicking the sign in link", () => {
+  it("Should show the login modal when clicking the sign in link", () => {
     cy.contains("Sign In with your Secret Key").click();
     cy.contains("Sign in with a secret Key");
     cy.contains("Sign In");
   });
 
-  it("should close de login modal when clicking the close button", () => {
+  it("Should close de login modal when clicking the close button", () => {
     cy.contains("Sign In with your Secret Key").click();
     cy.get('#signin-button').should('exist');
     cy.get('#close-button').click();
