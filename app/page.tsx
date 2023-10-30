246ad90c4d800cefcf3bc0877fa6b4e15501add9
Matías Henríquez <matias.henriquez.dev@gmail.com>
2023-10-30 12:20:59 -0300

    Modify IDs attributes to follow the kebab-case format

--- app/page.tsx
+++ app/page.tsx
@@ -44,7 +44,7 @@ const Index: FC = () => {
   }
 
   return (
-    <div className="grid grid-cols-1 grid-rows-6 h-screen bg-cyan-950">
+    <div id="home-container" className="grid grid-cols-1 grid-rows-6 h-screen bg-cyan-950">
       <nav className="relative flex w-full flex-wrap items-center justify-between py-2 text-neutral-500 shadow-lg hover:text-neutral-700 focus:text-neutral-700  lg:py-4">
         <div className="flex w-full flex-wrap items-center justify-between px-3">
           <div className="ml-10">
@@ -73,7 +73,7 @@ const Index: FC = () => {
             className="cursor-pointer underline underline-offset-8"
             onClick={handleCreateClick}
             data-te-toggle="modal"
-            data-te-target="#InfoModal"
+            data-te-target="#info-modal"
             data-te-ripple-init
             data-te-ripple-color="light"
           >
