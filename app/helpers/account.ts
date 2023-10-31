ccd609986728e59811b4beb2d8645be668730e66
Matías Henríquez <matias.henriquez.dev@gmail.com>
2023-10-31 09:45:13 -0300

    Add account helpers to load an account an get its balance

---
+++ app/helpers/account.ts
@@ -0,0 +1,28 @@
+import { Server } from "stellar-sdk";
+const server: Server = new Server("https://horizon-testnet.stellar.org");
+
+const loadAccount = async (publicKey: string) => {
+  try {
+    const account = await server.loadAccount(publicKey);
+    return account;
+  } catch (error) {
+    throw new Error("Unfunded account");
+  }
+};
+
+const getBalance = async (publicKey: string): Promise<string | undefined> => {
+  let balance: string | undefined = "0";
+
+  try {
+    const account = await loadAccount(publicKey);
+    balance = account.balances.find(
+      (balance) => balance.asset_type === "native"
+    )?.balance;
+  } catch (error) {
+    throw Error("Unfunded account");
+  }
+
+  return balance;
+};
+
+export default { loadAccount, getBalance };
