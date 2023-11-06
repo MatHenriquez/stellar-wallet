0d45b626e72d26cf8225b0a2c9029e578946b988
Matías Henríquez <matias.henriquez.dev@gmail.com>
2023-11-06 17:38:07 -0300

    Modify the sendPayments helper to use async/await instead of .then

--- app/helpers/payments.ts
+++ app/helpers/payments.ts
@@ -2,21 +2,15 @@ import {
   Server,
   Keypair,
   Transaction,
-  NotFoundError,
   Asset,
   Memo,
   Networks,
   Operation,
   TransactionBuilder,
   BASE_FEE,
+  KeypairType,
 } from "stellar-sdk";
 import { IPaymentSummary } from "../interfaces/payments";
-
-function handleError(error: Error) {
-  if (error instanceof NotFoundError) {
-    throw new Error("The destination account does not exist!");
-  } else return error;
-}
 
 const server = new Server(process.env.NEXT_PUBLIC_SERVER_URL || "");
 
@@ -28,7 +22,38 @@ const submitTransaction = (transaction: Transaction) =>
 
 const loadAccount = async (publicKey: string) => server.loadAccount(publicKey);
 
-export const sendPayment: (paymentSummary: IPaymentSummary) => void = ({
+const buildTransaction = async (
+  destinationPublicKey: string,
+  sourceKeys: Keypair,
+  transaction: Transaction,
+  fee: number,
+  amount: string,
+  memo: string,
+  timeOutInSeconds: number
+) => {
+  await loadAccount(destinationPublicKey);
+  const sourceAccount = await loadAccount(sourceKeys.publicKey());
+
+  transaction = new TransactionBuilder(sourceAccount, {
+    fee: fee.toString(),
+    networkPassphrase: Networks.TESTNET,
+  })
+    .addOperation(
+      Operation.payment({
+        destination: destinationPublicKey,
+        asset: Asset.native(),
+        amount: amount,
+      })
+    )
+    .addMemo(Memo.text(memo))
+    .setTimeout(timeOutInSeconds)
+    .build();
+
+  signTransaction(transaction, sourceKeys);
+  await submitTransaction(transaction);
+};
+
+export const sendPayment: (paymentSummary: IPaymentSummary) => void = async ({
   signerKey,
   destinationPublicKey,
   amount,
@@ -37,48 +62,44 @@ export const sendPayment: (paymentSummary: IPaymentSummary) => void = ({
   fee,
 }) => {
   const sourceKeys: Keypair = Keypair.fromSecret(signerKey);
-  let transaction: Transaction;
 
   if (fee < +BASE_FEE) throw new Error(`Fee cannot be less than ${BASE_FEE}`);
 
-  loadAccount(destinationPublicKey)
-    .catch((error: Error) => handleError(error))
-    .then(function () {
-      return loadAccount(sourceKeys.publicKey());
+  try {
+    await loadAccount(destinationPublicKey);
+    const sourceAccount = await loadAccount(sourceKeys.publicKey());
+
+    const transaction = new TransactionBuilder(sourceAccount, {
+      fee: fee.toString(),
+      networkPassphrase: Networks.TESTNET,
     })
-    .then(function (sourceAccount) {
-      transaction = new TransactionBuilder(sourceAccount, {
-        fee: fee.toString(),
-        networkPassphrase: Networks.TESTNET,
-      })
-        .addOperation(
-          Operation.payment({
-            destination: destinationPublicKey,
-            asset: Asset.native(),
-            amount: amount,
-          })
-        )
-        .addMemo(Memo.text(memo))
-        .setTimeout(timeOutInSeconds)
-        .build();
-      signTransaction(transaction, sourceKeys);
-      return submitTransaction(transaction);
-    })
-    .then(function () {
-      return {
-        sourcePublicKey: sourceKeys.publicKey(),
-        destinationPublicKey: destinationPublicKey,
-        amount: amount,
-        status: "Success",
-      };
-    })
-    .catch(function (error: Error) {
-      return {
-        sourcePublicKey: sourceKeys.publicKey(),
-        destinationPublicKey: destinationPublicKey,
-        amount: amount,
-        status: "Failed",
-        errorMessage: error.message,
-      };
-    });
+      .addOperation(
+        Operation.payment({
+          destination: destinationPublicKey,
+          asset: Asset.native(),
+          amount: amount,
+        })
+      )
+      .addMemo(Memo.text(memo))
+      .setTimeout(timeOutInSeconds)
+      .build();
+
+    signTransaction(transaction, sourceKeys);
+    await submitTransaction(transaction);
+
+    return {
+      sourcePublicKey: sourceKeys.publicKey(),
+      destinationPublicKey: destinationPublicKey,
+      amount: amount,
+      status: "Success",
+    };
+  } catch (error: any) {
+    return {
+      sourcePublicKey: sourceKeys.publicKey(),
+      destinationPublicKey: destinationPublicKey,
+      amount: amount,
+      status: "Failed",
+      errorMessage: error.message,
+    };
+  }
 };
