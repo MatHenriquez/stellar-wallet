a3d7309132c9b942f149026cb107a607e3f6dfa2
Matías Henríquez <matias.henriquez.dev@gmail.com>
2023-11-13 15:32:18 -0300

    Modify the home tests to adapt them to the new login option

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -1,7 +1,7 @@
 import { generateKeys } from "../../app/helpers/generateKeys";
 
-describe("End-to-End testing", () => {
-  describe("Home page", () => {
+describe("Home page", () => {
+  describe("Components", () => {
     beforeEach(() => {
       cy.visit("/");
     });
@@ -18,10 +18,6 @@ describe("End-to-End testing", () => {
       cy.get('[href="#"]').should("have.text", "My_Wallet");
     });
 
-    it("Should have a link to sign in with a secret key", () => {
-      cy.contains("Sign In with your Secret Key");
-    });
-
     it("Should have a link to create new keys", () => {
       cy.get('[data-te-toggle="modal"]').should(
         "have.text",
@@ -34,39 +30,41 @@ describe("End-to-End testing", () => {
       cy.contains("Public Key: ");
       cy.contains("Secret Key: ");
     });
-
-    it("Should show the login modal when clicking the sign in link", () => {
-      cy.contains("Sign In with your Secret Key").click();
-      cy.contains("Sign in with a secret Key");
-      cy.contains("Sign In");
-    });
-
-    it("Should close de login modal when clicking the close button", () => {
-      cy.contains("Sign In with your Secret Key").click();
-      cy.get("#signin-button").should("exist");
-      cy.get("#close-button").click();
-      cy.get("#login-modal").should("not.exist");
-    });
   });
 
-  describe("Login", () => {
+  describe("Login with secret key", () => {
     const secretKey: string = generateKeys().secret();
 
     beforeEach(() => {
       cy.visit("/");
     });
 
-    it("Should be unable to log in with an invalid secret key", () => {
-      cy.contains("Sign In with your Secret Key").click();
-      cy.get("#signin-button").click();
-      cy.get("#error-message").should("have.text", "Invalid secret key");
+    it("Should have a button to log in with a secret key", () => {
+      cy.get('[data-cy="secretKey-login-button"]').should("exist");
     });
 
-    it("Should be able to log in with a valid secret key", () => {
-      cy.contains("Sign In with your Secret Key").click();
-      cy.get('[type="password"]').type(secretKey);
-      cy.get("#signin-button").click();
-      cy.get("#error-message").should("not.exist");
+    it("Should have an input field for the secret key", () => {
+      cy.get('[data-cy="secret-key-input"]').should("exist");
+    });
+
+    it("Should show an error message when the user tries to login with an invalid secret key", () => {
+      cy.get('[data-cy="secret-key-input"]').type("fake_secret_key");
+      cy.get('[data-cy="secretKey-login-button"]').click();
+      cy.get('[data-cy="login-error-message"]').should("exist");
+    });
+
+    it("Should hide the error message when the user clicks on the error message close button", () => {
+      cy.get('[data-cy="secret-key-input"]').type("fake_secret_key");
+      cy.get('[data-cy="secretKey-login-button"]').click();
+      cy.get('[data-cy="login-error-message"]').should("exist");
+      cy.get('[data-cy="error-message-close-button"]').click();
+      cy.get('[data-cy="login-error-message"]').should("not.exist");
+    });
+
+    it("Should redirect to the dashboard page after logging in with a valid secret key", () => {
+      cy.get('[data-cy="secret-key-input"]').type(secretKey);
+      cy.get('[data-cy="secretKey-login-button"]').click();
+      cy.url().should("include", "/dashboard");
     });
   });
 
