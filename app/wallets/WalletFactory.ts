f9ade8bd607102cd6cc44441d125792c19b30947
Matías Henríquez <matias.henriquez.dev@gmail.com>
2023-11-13 15:12:26 -0300

    Add the secret key wallet class to the wallet factory

--- app/wallets/WalletFactory.ts
+++ app/wallets/WalletFactory.ts
@@ -2,10 +2,11 @@ import LocalStorage from "../helpers/storage";
 import type IWallet from "../interfaces/wallet";
 import InvalidWalletError from "./errors/InvalidWallet";
 import Albedo from "./albedo/Albedo";
+import SecretKey from "./secret-key/SecretKey";
 
 export default class WalletFactory {
   createAll(): IWallet[] {
-    return [Albedo.NAME].map(this.create);
+    return [Albedo.NAME, SecretKey.NAME].map(this.create);
   }
 
   create(name: string) {
@@ -14,6 +15,9 @@ export default class WalletFactory {
     switch (name) {
       case Albedo.NAME:
         wallet = new Albedo(storage);
+        break;
+      case SecretKey.NAME:
+        wallet = new SecretKey(storage);
         break;
       default:
         throw new InvalidWalletError();
