e1fe91b9b317e5d0c97c5b4cf6f3f51df291a552
Matías Henríquez <matias.henriquez.dev@gmail.com>
2023-11-14 14:56:52 -0300

    Add new functions in the payments helper file for signing with available wallets

--- app/helpers/payments.ts
+++ app/helpers/payments.ts
@@ -8,16 +8,23 @@ import {
   Operation,
   TransactionBuilder,
   BASE_FEE,
+  xdr,
 } from "stellar-sdk";
 import { IPaymentSummary } from "../interfaces/payments";
+import IWallet from "@component/interfaces/wallet";
 
 const server = new Server(process.env.NEXT_PUBLIC_SERVER_URL || "");
 
 const signTransaction = (transaction: Transaction, sourceKeys: Keypair) =>
   transaction.sign(sourceKeys);
 
-const submitTransaction = (transaction: Transaction) =>
-  server.submitTransaction(transaction);
+export const submitTransaction = async (transaction: Transaction) => {
+  try {
+    await server.submitTransaction(transaction);
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 const loadAccount = async (publicKey: string) => server.loadAccount(publicKey);
 
@@ -25,32 +32,71 @@ export const sendPayment: (paymentSummary: IPaymentSummary) => void = async ({
   signerKey,
   destinationPublicKey,
   amount,
-  memo
+  memo,
 }) => {
   const sourceKeys: Keypair = Keypair.fromSecret(signerKey);
-  const timeOutInSeconds: number = 60;
 
   try {
     await loadAccount(destinationPublicKey);
-    const sourceAccount = await loadAccount(sourceKeys.publicKey());
 
-    const transaction = new TransactionBuilder(sourceAccount, {
-      fee: BASE_FEE,
-      networkPassphrase: Networks.TESTNET,
-    })
-      .addOperation(
-        Operation.payment({
-          destination: destinationPublicKey,
-          asset: Asset.native(),
-          amount: amount,
-        })
-      )
-      .addMemo(Memo.text(memo))
-      .setTimeout(timeOutInSeconds)
-      .build();
+    const transaction = await buildTransaction(
+      sourceKeys.publicKey(),
+      destinationPublicKey,
+      amount,
+      memo
+    );
 
     signTransaction(transaction, sourceKeys);
     await submitTransaction(transaction);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const buildTransaction = async (
+  sourcePublicKey: string,
+  destinationPublicKey: string,
+  amount: string,
+  memo: string
+) => {
+  const sourceAccount = await loadAccount(sourcePublicKey);
+
+  const transaction = new TransactionBuilder(sourceAccount, {
+    fee: BASE_FEE,
+    networkPassphrase: Networks.TESTNET,
+  })
+    .addOperation(
+      Operation.payment({
+        destination: destinationPublicKey,
+        asset: Asset.native(),
+        amount: amount,
+      })
+    )
+    .addMemo(Memo.text(memo))
+    .setTimeout(60)
+    .build();
+
+  return transaction;
+};
+
+export const signTransactionWithWallet = async (
+  transaction: Transaction,
+  wallet: IWallet
+) => {
+  try {
+    return await wallet.sign(transaction);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const getTransactionFromXdr = (transactionXdr: string) => {
+  try {
+    const transactionEnvelope = xdr.TransactionEnvelope.fromXDR(
+      transactionXdr,
+      "base64"
+    );
+    return new Transaction(transactionEnvelope, Networks.TESTNET);
   } catch (error) {
     console.error(error);
   }
